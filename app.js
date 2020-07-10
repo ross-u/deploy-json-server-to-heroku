d9b766a74fe84773fba965575f403cdbefff7475
ross-u <cirkovic.uros.srb@gmail.com>
2020-07-10 16:42:02 +0200

    Bugfix - add custom middleware to disable CORS

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const morgan = require("morgan");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const options = { noCors: true }; // Disables CORS
-const middlewares = jsonServer.defaults(options);
+const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT;
 
 server.use(middlewares);
 server.use(morgan("dev"));
+server.use((req, res, next) => { // Middleware to disable CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+})
 server.use(router);
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running at port ${PORT}`);
 });
+
